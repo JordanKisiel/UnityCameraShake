d880b3b95035e7171b385e1a6313dd0dc9a85b24
Jordan Kisiel <jordan.kisiel@gmail.com>
2016-06-08 18:13:28 -0400

    Added CameraShake and Test scripts

---
+++ CameraShake.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+
+	private Camera thisCamera;
+
+	void Start(){
+		thisCamera = gameObject.GetComponent<Camera>();
+	}
+	
+	public void Shake(float amplitude, float duration){
+		StopAllCoroutines();
+		StartCoroutine(ShakeCamera(amplitude, duration));
+	}
+
+	private IEnumerator ShakeCamera(float amplitude, float duration, float dampStartPercentage = 0.75f){
+		//ensure percentage is in a valid range
+		dampStartPercentage = Mathf.Clamp(dampStartPercentage, 0.0f, 1.0f);
+
+		float elapsedTime = 0.0f;
+		float damp = 1.0f;
+
+		Vector3 cameraOrigin = thisCamera.transform.position;
+
+		while(elapsedTime < duration){
+
+			elapsedTime += Time.deltaTime;
+
+			float percentComplete = elapsedTime / duration;
+
+			if(percentComplete >= dampStartPercentage && percentComplete <= 1.0f){
+				damp = 1.0f - percentComplete;
+			}
+
+			Vector2 offsetValues = Random.insideUnitCircle;
+
+			offsetValues *= amplitude * damp;
+
+			thisCamera.transform.position = new Vector3(offsetValues.x, offsetValues.y, cameraOrigin.z);
+
+			yield return null;
+		}
+
+		thisCamera.transform.position = cameraOrigin;
+	}
+}
---
+++ TestCameraShake.cs
@@ -0,0 +1,37 @@
+﻿/*
+	Attach this to the same camera as you attached the
+	CameraShake script to test and experiment with
+	different values.
+*/
+
+using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(CameraShake))]
+public class TestCameraShake : MonoBehaviour {
+
+	public float amplitude = 0.4f;
+	public float duration = 0.5f;
+	//uncomment the following line and pass it in the Shake call in update if you want
+	//to change when the damping starts
+	//public float dampStart = 0.75f; 
+	public bool testShake = false;
+
+	private CameraShake cameraShake;
+
+	// Use this for initialization
+	void Start () {
+		cameraShake = gameObject.GetComponent<CameraShake>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(testShake){
+			testShake = false;
+			//there's an optional third parameter available for those that want to
+			//experiment with when the damping starts
+			cameraShake.Shake(amplitude, duration);
+		}
+	}
+}
